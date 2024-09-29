408ea6dc11c3676f4ea9e64ea0557fa5a208e90a
Darlito <max.grejon@icloud.com>
2024-09-29 17:44:44 +0200

    etape 4 : créer les conditions

--- Calculatrice/main.c
+++ Calculatrice/main.c
@@ -6,11 +6,40 @@ int main()
 	int number_one;
 	int number_two;
 	char operation;
-	printf("Bonjour, quel est l'opération que vous souhaitez faire ?: +, -, *, /, %");
+	char addition = '+';
+	char soustraction = '-';
+	char multiplication = '*';
+	char division = '/';
+	char modulo = '%';
+	printf("Bonjour, quel est l'opération que vous souhaitez faire ?: +, -, *, /, %%\n");
 	scanf("%c", &operation);
-	printf("Veuillez saisir votre premier nombre");
+	printf("Veuillez saisir votre premier nombre\n");
 	scanf("%d", &number_one);
-        printf("Veuillez maintenant saisir votre deuxième nombre");
+        printf("Veuillez maintenant saisir votre deuxième nombre\n");
         scanf("%d", &number_two);
+        if (operation == addition)
+        {
+                printf("addition\n");
+        }
+        else if (operation == soustraction)
+        {
+                printf("soustraction\n");
+        }
+        else if (operation == multiplication)
+        {
+                printf("multiplication\n");
+        }
+        else if (operation == division)
+        {
+                printf("division\n");
+        }
+        else if (operation == modulo)
+        {
+                printf("modulo\n");
+        }
+        else
+        {
+                printf("L'opérateur '%c' n'est pas reconnu\n", operation);
+        }
 	exit(0);
 }
