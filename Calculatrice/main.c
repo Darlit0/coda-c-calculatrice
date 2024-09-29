673c8913d59bddef5d9d5d44452fd2ee383e10ca
Darlito <max.grejon@icloud.com>
2024-09-29 15:50:25 +0200

    etape 3 : recupérer les infos

--- Calculatrice/main.c
+++ Calculatrice/main.c
@@ -6,5 +6,11 @@ int main()
 	int number_one;
 	int number_two;
 	char operation;
+	printf("Bonjour, quel est l'opération que vous souhaitez faire ?: +, -, *, /, %");
+	scanf("%c", &operation);
+	printf("Veuillez saisir votre premier nombre");
+	scanf("%d", &number_one);
+        printf("Veuillez maintenant saisir votre deuxième nombre");
+        scanf("%d", &number_two);
 	exit(0);
 }
