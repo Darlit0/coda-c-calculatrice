640962bf0e7ffe99a56acafffaf12aa14868ac6b
Darlito <max.grejon@icloud.com>
2024-09-29 22:05:17 +0200

    etape 5 : l'opération

--- Calculatrice/main.c
+++ Calculatrice/main.c
@@ -11,6 +11,7 @@ int main()
 	char multiplication = '*';
 	char division = '/';
 	char modulo = '%';
+        int resultat;
 	printf("Bonjour, quel est l'opération que vous souhaitez faire ?: +, -, *, /, %%\n");
 	scanf("%c", &operation);
 	printf("Veuillez saisir votre premier nombre\n");
@@ -19,23 +20,43 @@ int main()
         scanf("%d", &number_two);
         if (operation == addition)
         {
+		int resultat = number_one + number_two;
                 printf("addition\n");
         }
         else if (operation == soustraction)
         {
+		int resultat = number_one - number_two;
                 printf("soustraction\n");
         }
         else if (operation == multiplication)
         {
+		int resultat = number_one * number_two;
                 printf("multiplication\n");
         }
         else if (operation == division)
         {
-                printf("division\n");
+		if (number_two != 0)
+		{
+			int resultat = number_one / number_two;
+                	printf("division\n");
+		}
+		else
+		{
+			printf("Il est impossible de diviser par 0");
+		}
         }
         else if (operation == modulo)
         {
-                printf("modulo\n");
+		if (number_two != 0)
+		{
+			int resultat = number_one % number_two;
+                	printf("modulo\n");
+		}
+		else
+		{
+			printf("Il est impossible de diviser par 0");
+		}
+
         }
         else
         {
@@ -43,3 +64,4 @@ int main()
         }
 	exit(0);
 }
+
